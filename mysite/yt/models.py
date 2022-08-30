7bdfd3e5bc3669c34f7b13e4967d1ec54b543d07
SanjaySinghRajpoot <iamsanjayrajpoot@gmail.com>
2022-08-30 16:10:43 +0530

    typo fix

--- mysite/yt/models.py
+++ mysite/yt/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class video_data(models.Model):
-    video_title = models.CharField(max_length=500)
-    descrip = models.CharField(max_length=5000)
+    title = models.CharField(max_length=500)
+    description = models.CharField(max_length=5000)
 
     
